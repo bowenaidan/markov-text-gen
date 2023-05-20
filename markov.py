ce978a38b2b1c07adbb1f5086666a6484a416981
Aidan <rivott35@hotmail.com>
2023-05-19 23:12:54 -0500

    added comments

--- markov.py
+++ markov.py
@@ -6,17 +6,20 @@ def genMarkov(input):
     markov = {}
     words = input.split(" ")
     index = 1
-    window = index
+    size = index
 
     for word in words[index:]:
-        key = " ".join(words[(index-window):index])
-        if key in markov:
-            if word in markov[key]:
-                markov[key][word] += 1
+        window = " ".join(words[(index-size):index])
+        if window in markov:
+            # each window is a key for words
+            # nested dictionary have words that are keys
+            # words have weights
+            if word in markov[window]:
+                markov[window][word] += 1
             else:
-                markov[key][word] = 1
+                markov[window][word] = 1
         else:
-            markov[key] = {word: 1}
+            markov[window] = {word: 1}
         index += 1
     return markov
 
@@ -25,16 +28,20 @@ def genText(markov):
     words = random.choice(list(markov.keys())).split(" ")
     text = " ".join(words) + " "
     for _ in range(50):
-        key = " ".join(words)
-        if key in markov:
-            next_words = markov[key]
+        # joins the words to make a window
+        window = " ".join(words)
+        if window in markov:
+            # checks the words in the window currently
+            next_words = markov[window]
             choices = []
             weights = []
+            #sets the choices and weights
             for word, weight in next_words.items():
                 choices.append(word)
                 weights.append(weight)
             addition = random.choices(choices, weights=weights)[0]
             text += addition + " "
+            # slides the window up so it contains the new word
             words.pop(0)
             words.append(addition)
         else:
